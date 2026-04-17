e49e9ca6be3ca66c3b76ec213e48ce42bc5eb77a
Sala Digital (CtP/ETS) <disrct@bosch.com>
2026-04-17 13:28:33 -0300

    configured router outlet, defined routes

--- message-app/src/app/app.routes.ts
+++ message-app/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Subscribe } from './features/subscribe/subscribe';
+import { Login } from './features/login/login';
+import { AuthPage } from './features/auth-page/auth-page';
+import { Chat } from './features/chat/chat';
+import { ContactsList } from './features/contacts-list/contacts-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'register', component: Subscribe },
+    { path: 'login', component: Login },
+    { path: '', component: AuthPage },
+    { path: 'chat', component: Chat },
+    { path: 'contacts', component: ContactsList }
+];
