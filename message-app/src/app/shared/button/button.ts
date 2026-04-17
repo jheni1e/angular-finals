2419f7344af3a592659e177ce5c3c95de29add21
Jhenie <highhalma@gmail.com>
2026-04-17 13:33:37 -0300

    implemented button component

--- message-app/src/app/shared/button/button.ts
+++ message-app/src/app/shared/button/button.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -9,4 +9,13 @@ import { Component } from '@angular/core';
 export class Button {
   // O componente de botão deve receber uma binding de input com o nome de 'title' e de um output com nome de 'onClick' onde o title deverá ser o unico texto
   // aparecendo no botão e o onClick deverá ativar o evento para o componente pai.
+
+  @Input() title: string = '';
+  @Input() action!: () => void;
+
+  onClick = () => {
+    if (this.action) {
+      this.action();
+    }
+  }
 }
