86be5f6f894ebba95bb4dd3801f16688211bd05d
Jhenie <highhalma@gmail.com>
2026-04-17 14:07:42 -0300

    input in progress, testing in auth page

--- message-app/src/app/features/auth-page/auth-page.ts
+++ message-app/src/app/features/auth-page/auth-page.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { Header } from '../../shared/header/header';
+import { InputComponent } from '../../shared/input/input';
 
 @Component({
   selector: 'app-auth-page',
-  imports: [Header],
+  imports: [Header, InputComponent],
   templateUrl: './auth-page.html',
   styleUrl: './auth-page.css',
 })
 export class AuthPage {
   // Nesta página iremos trabalhar com a autenticação do usuário onde teremos uma sub-página (child) para login e uma para se inscrever na aplicação.
+
+  text: string = "";
+
+  changed = (e: string) => {
+    this.text = e;
+  }
 }
--- message-app/src/app/shared/input/input.ts
+++ message-app/src/app/shared/input/input.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-input',
@@ -6,9 +6,18 @@ import { Component } from '@angular/core';
   templateUrl: './input.html',
   styleUrl: './input.css',
 })
-export class Input {
+export class InputComponent {
   // O componente input deverá ter as seguintes bindings
   // value: uma binding que recebe o valor inicial, e será a mesma que armazena o conteúdo das atualizaçoes dos textos.
   // change: uma binding que envia ao pai o valor a cada alteração do usuário
   // placeholder: uma binding que recebe um texto que irá ficar disponível enquanto não tem valor nenhum
+
+  @Input() value: string = '';
+  @Output() change: EventEmitter<string> = new EventEmitter();
+  @Input() placeholder: string = '';
+
+  onChange = (e: Event) => {
+    const value = (e.target as HTMLInputElement).value;
+    this.change.emit(value);
+  }
 }
